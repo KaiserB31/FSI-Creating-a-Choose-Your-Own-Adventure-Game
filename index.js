b2f53e45fa0231c1fb2ff4b19e2422cf2726c746
KaiserB31 <kaiserbryan0@gmail.com>
2022-10-20 11:46:32 -0700

    made changes

--- index.js
+++ index.js
@@ -15,12 +15,10 @@ Do you follow it, or continue on your path?`)
 if(firstAnswer === 'left')
 {
 let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
-
+}
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
 
 }
-
-   
