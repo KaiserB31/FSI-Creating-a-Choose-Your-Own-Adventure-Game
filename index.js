fe10e9f8c6d182a9cc92cd1b7a09ac11fccd2f44
KaiserB31 <kaiserbryan0@gmail.com>
2022-10-20 11:20:46 -0700

    Checking process

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ if(firstAnswer === 'left')
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
+if(firstAnswer === 'left')
+{
+let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
 
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
@@ -19,3 +22,5 @@ On the other side of him, you see a shiny chest of treasure. Another path would
 lead you away from the dragon altogether. Which path do you take?`)
 
 }
+
+   
