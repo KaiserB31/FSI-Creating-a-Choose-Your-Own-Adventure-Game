02ac2e8a0c7e88c1a05cb8dbb7db366409604a9d
KaiserB31 <kaiserbryan0@gmail.com>
2022-10-20 08:59:29 -0700

    "changed project"

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 window.prompt('Do you head left or right?')
 let answer  = window.prompt('Do you head left or right?')
-let firstAnswer  = window.prompt('Do you head left or right?')
-{
-console.log(firstAnswer)
-let firstAnswer  = window.prompt('Do you head left or right?')
-console.log(firstAnswer === 'left')
-}
+
