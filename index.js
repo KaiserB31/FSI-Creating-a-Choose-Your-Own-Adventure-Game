4d3d89e4f638f6a7e1e797abe5fe4e9a0f4b8f9c
KaiserB31 <kaiserbryan0@gmail.com>
2022-10-19 20:23:50 -0700

    made changes

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 window.prompt('Do you head left or right?')
-let answer = window.prompt('Do you head left or right')
-let firstAnswer = window.prompt('Do you head left or right?')
-console.log(firstAnswer)
-let firstAnswer = window.prompt('Do you head left or right?')
-console.log(firstAnswer === 'left')
